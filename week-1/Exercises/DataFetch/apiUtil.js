1dbb8c21594a8da42571bcea9307a15342aa9f6b
dandevri <hi@dandevri.es>
2024-10-15 16:06:02 +0200

    Update apiUtil

--- week-1/Exercises/DataFetch/apiUtil.js
+++ week-1/Exercises/DataFetch/apiUtil.js
@@ -1,19 +1,7 @@
-// Write a module that fetches a dataset e.g. 'fake' posts from Json Placeholder. https://jsonplaceholder.typicode.com/posts
+// Write a module that fetches a dataset e.g. 'fake' posts from Json Placeholder. https://jsonplaceholder.typicode.com/posts Use async await.
 
 async function fetchPosts() {
   const url = "https://jsonplaceholder.typicode.com/posts";
-
-  try {
-    const response = await fetch(url);
-    if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
-    }
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.error("Error fetching posts:", error);
-    return [];
-  }
 }
 
 // Export the functions using named exports.

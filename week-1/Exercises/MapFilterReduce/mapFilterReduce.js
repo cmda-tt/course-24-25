37424e3efb48393e355e62b26f81f8eb80bd9c3c
Laura Benvenuti <l.benvenuti@hva.nl>
2024-10-15 10:25:58 +0200

    Exercises week1 - tuesday

---
+++ week-1/Exercises/MapFilterReduce/mapFilterReduce.js
@@ -0,0 +1,68 @@
+const data = [
+    {
+        name: "robert",
+        age: "29",
+        residence: "amsterdam",
+        siblings: 1,
+        work: {
+            title: "Lecturer",
+            employer: "Hogeschool van Amsterdam"
+        }
+    },
+    {
+        name: "berend",
+        age: "32",
+        residence: "rotterdam",
+        siblings: 0,
+        work: {
+            title: "Front-end Developer",
+            employer: "DEPT"
+        }
+    },
+    {
+        name: "ubaida",
+        age: "26",
+        residence: "Amersfoort",
+        siblings: 4,
+        work: {
+            title: "Project Manager",
+            employer: "Clarify"
+        }
+    }
+];
+
+
+
+/* Filter by age, normalize capitals in names, convert ages to numbers, remove work.
+    const data = [
+        {
+            name: "Robert",
+            age: 29,
+            siblings: 1,
+            residence: "Amsterdam",
+        },
+        {
+            name: "Berend",
+            age: 32,
+            siblings: 0,
+            residence: "Rotterdam",
+        }
+    ];
+*/
+
+// hulpfunctie die objecten uit array data transformeret naar het gewenste formaat
+// en het object teruggeeft
+function transformPerson(person) {
+
+}
+
+// transformeer array data met behulp van de map-functie en sla het resultaat op 
+// in varabele transformedData
+
+// toon in de console een array met alle de personen, ouder dan 26
+
+// laat de som zien van van de broertjes/zusjes van personen, ouder dan 26
+// console.log(filteredData.reduce((acc, item) => acc + item.siblings, 0));
+
+
+// In één statement
---
+++ week-1/Exercises/ObjectsMapFilter/objectsMapFilter.js
@@ -0,0 +1,65 @@
+const data = [
+    {
+        name: "robert",
+        age: "29",
+        residence: "amsterdam",
+        siblings: 1,
+        work: {
+            title: "Lecturer",
+            employer: "Hogeschool van Amsterdam"
+        }
+    },
+    {
+        name: "berend",
+        age: "32",
+        residence: "rotterdam",
+        siblings: 0,
+        work: {
+            title: "Front-end Developer",
+            employer: "DEPT"
+        }
+    },
+    {
+        name: "ubaida",
+        age: "26",
+        residence: "Amersfoort",
+        siblings: 4,
+        work: {
+            title: "Project Manager",
+            employer: "Clarify"
+        }
+    }
+];
+
+
+
+/* Filter by age, normalize capitals in names, convert ages to numbers, remove work.
+    const data = [
+        {
+            name: "Robert",
+            age: 29,
+            residence: "Amsterdam",
+        },
+        {
+            name: "Berend",
+            age: 32,
+            residence: "Rotterdam",
+        }
+    ];
+*/
+
+// hulpfunctie die objecten uit array data transformeret naar het gewenste formaat
+// en het object teruggeeft
+function transformPerson(myPerson) {
+
+
+}
+
+// toon het object dat Berend representeert in de console
+
+
+// transformeer het hele array met de map-functie, en toon het resultaat
+
+
+// transformeer het hele array met de map-functie, en toon de objecten
+// die personen representeren die ouder zijn dan 26
